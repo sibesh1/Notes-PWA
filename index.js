fcd3d849076af6bb22ad2d22c47eac8c6b67b980
sibesh1 <sibesh1998@gmail.com>
2020-12-14 10:26:30 +0530

    refactored backend

--- index.js
+++ index.js
@@ -1,101 +1,10 @@
-//MIDDLEWARE
-require("dotenv").config();
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-const Note = require("./models/mongo");
-const cors = require("cors");
-app.use(cors()); //CORS validation
-app.use(express.json());
-app.use(bodyParser.json());
-app.use(express.static("build")); //Frontend Page
+const app = require("./app"); // varsinainen Express-sovellus
+const http = require("http");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
 
-//GET REQUESTS
-app.get("/api/notes", (request, response) => {
-  Note.find({}).then((note) => {
-    response.json(note);
-  });
-});
+const server = http.createServer(app);
 
-app.get("/api/notes/:id", (request, response) => {
-  Note.findById(request.params.id)
-    .then((note) => {
-      if (note) {
-        response.json(note);
-      } else {
-        response.status(404).end();
-      }
-    })
-    .catch((error) => next(error));
-});
-
-//POST REQUESTS
-app.post("/api/notes", (request, response) => {
-  const body = request.body;
-
-  if (body.content === undefined) {
-    return response.status(400).json({ error: "content missing" });
-  }
-
-  const note = new Note({
-    content: body.content,
-    important: body.important || Math.random() < 0.5,
-    date: new Date(),
-  });
-
-  note
-    .save()
-    .then((savedNote) => {
-      response.json(savedNote);
-    })
-    .catch((error) => next(error));
-});
-
-//UPDATE IMPORTANT OR NOT IMPORTANT REQUESTS
-app.put("/api/notes/:id", (request, response, next) => {
-  const body = request.body;
-
-  const note = {
-    content: body.content,
-    important: body.important,
-  };
-
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
-    .then((updatedNote) => {
-      response.json(updatedNote);
-    })
-    .catch((error) => next(error));
-});
-
-//DELETE REQUESTS
-app.delete("/api/notes/:id", (request, response, next) => {
-  Note.findByIdAndRemove(request.params.id)
-    .then((result) => {
-      response.status(204).end();
-    })
-    .catch((error) => next(error));
-});
-
-//ERROR HANDLING
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
-app.use(unknownEndpoint);
-
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
-
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message });
-  }
-
-  next(error);
-};
-app.use(errorHandler);
-
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
